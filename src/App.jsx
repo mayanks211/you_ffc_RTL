5aebd1f381419e991d5565b34bda04f2800d354e
mayank211 <mayanksrivastava21@gmail.com>
2026-09-20 15:41:06 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
